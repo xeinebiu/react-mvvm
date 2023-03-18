99bf302a1d30b4c104f78a99521c5ebb35df725c
xeinebiu <xeinebiu@gmail.com>
2023-03-18 19:08:58 +0100

    generate react-mvvm library

---
+++ libs/react-mvvm/src/lib/react-mvvm.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ReactMvvmProps {}
+
+export function ReactMvvm(props: ReactMvvmProps) {
+    return (
+        <div>
+            <h1>Welcome to ReactMvvm!</h1>
+        </div>
+    );
+}
+
+export default ReactMvvm;
