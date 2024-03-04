7e9792dcb44e3ad77d27ea29f3f712a2ef430630
xeinebiu <xeinebiu@gmail.com>
2024-03-04 11:47:02 +0100

    add useRememberState for global state management

--- libs/react-mvvm/src/index.ts
+++ libs/react-mvvm/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/mvvm';
+export * from './lib/state';
---
+++ libs/react-mvvm/src/lib/state.ts
@@ -0,0 +1,72 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+type ResetFunc = () => void;
+
+const globalState = new Map<string, any>();
+
+const globalStateListeners = new Map<string, (() => void)[]>();
+
+export function useRememberState<State>({
+    id,
+    initialState,
+}: {
+    id: string;
+    initialState: State;
+}): [State, Dispatch<SetStateAction<State>>, ResetFunc] {
+    const state = globalState.has(id) ? globalState.get(id) : initialState;
+
+    const [, setChangesCounter] = useState(0);
+
+    useEffect(() => {
+        const handler = () => {
+            setChangesCounter(prev => prev + 1);
+        };
+
+        if (!globalStateListeners.has(id)) {
+            globalStateListeners.set(id, []);
+        }
+
+        globalStateListeners.get(id)?.push(handler);
+
+        return () => {
+            const listeners = globalStateListeners.get(id);
+            if (listeners) {
+                globalStateListeners.set(
+                    id,
+                    listeners.filter(x => x !== handler),
+                );
+            }
+        };
+    }, [id]);
+
+    const setState = (newState: State | ((prev: State) => State)) => {
+        if (newState instanceof Function) {
+            const updatedState = newState(state);
+            globalState.set(id, updatedState);
+        } else {
+            globalState.set(id, newState);
+        }
+
+        // inform listeners
+        globalStateListeners.get(id)?.forEach(listener => {
+            listener();
+        });
+
+        // trigger re-render
+        setChangesCounter(prev => prev + 1);
+    };
+
+    const resetState = () => {
+        globalState.delete(id);
+
+        // inform listeners
+        globalStateListeners.get(id)?.forEach(listener => {
+            listener();
+        });
+
+        // trigger re-render
+        setChangesCounter(prev => prev + 1);
+    };
+
+    return [state, setState, resetState];
+}
